bbd82df3cc8803d00c706eec95f3b538c1247497
Code Proville Christophe <christophe.proville@gmail.com>
2026-08-21 13:00:13 +0200

    Autoriser port 5000 CORS et Helmet

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ app.use((err, req, res, next) => {
 
 // 7. Lancement du serveur
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`🐾 Serveur La Ferme d'Acq lancé sur http://localhost:${PORT}`);
 });
 
